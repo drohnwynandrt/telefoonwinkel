18a8dfd5f44868673ba83f53625528063eccf045
drohnwynandrt <dw.ramdattewari@gmail.com>
2019-06-27 09:34:21 +0200

    Added JS for the contact form to require input and give a response

--- telefoonwinkel.js
+++ telefoonwinkel.js
@@ -4,3 +4,23 @@ function navBar(){
 
 document.getElementById("navigationbar").innerHTML= navBarInner;
 }
+function completeOrder(){
+    var customerName = document.getElementById("customerNameField").value;
+    var customerEmail = document.getElementById("customerEmailField").value;
+
+    if (customerName == ""  ){
+        alert ("Please fill in your name")
+    }
+    else if (customerEmail == ""){
+        alert ("Please fill in your e-mail")
+    }
+    else  {
+        alert("Thank you "+ customerName+" for your purchase. We will contact you through e-mail!");
+        document.location.reload() ;
+    } 
+}
+
+function cancelOrder(){
+    document.location.reload() ;
+    
+}
