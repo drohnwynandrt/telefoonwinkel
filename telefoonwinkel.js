6e1cb15cd8182e79a68583382b5aa1f46f9ae6ed
drohnwynandrt <dw.ramdattewari@gmail.com>
2019-06-27 09:31:57 +0200

    Added bulma and a contact form

--- telefoonwinkel.js
+++ telefoonwinkel.js
@@ -1,6 +1,6 @@
 window.onload=navBar();
 function navBar(){
-    navBarInner="<a href='index.html'>Home</a> <a href='products.html'>Products</a>";
+    navBarInner="<a href='index.html'>Home</a> <a href='products.html'>Products</a> <a href='contact.html'>Contact</a>";
 
 document.getElementById("navigationbar").innerHTML= navBarInner;
 }
